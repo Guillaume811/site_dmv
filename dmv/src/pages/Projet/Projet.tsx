baf6daec529af894fa81979d75d0031213cfe1fa
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-04-24 17:13:43 +0200

    ajout section page projet

--- dmv/src/pages/Projet/Projet.tsx
+++ dmv/src/pages/Projet/Projet.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import HeadingSection from '../../components/sections/HeadingSection/HeadingSection';
+import BasicSection from '../../components/sections/BasicSection/BasicSection';
 
 const Projet: React.FC = () => {
     return (
         <div>
-            <h1>Projet</h1>
-            <p>Bienvenue sur la page Projet.</p>
+            {/*Section d'en-tête*/}
+            <HeadingSection title="Nos Projets" />
+
+            {/*Section de présentation des projets*/}
+            <BasicSection>
+                <p>
+                    Ici il y aura l'intégralité des cards projets.
+                    La page projet, comportera 3 sections, une en-tête, une section de présentation et une section galerie.
+                </p>
+            </BasicSection>
+
+                
         </div>
     );
 };
