70b6b47ca440e2e205e7abab7232660aa7ab746e
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-04-24 17:22:41 +0200

    ajout section page prestation

--- dmv/src/pages/Prestation/Prestation.tsx
+++ dmv/src/pages/Prestation/Prestation.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import HeadingSection from '../../components/sections/HeadingSection/HeadingSection';
+import Button from '../../components/Button/Button';
+import BasicSection from '../../components/sections/BasicSection/BasicSection';
 
 const Prestation: React.FC = () => {
     return (
         <div>
-            <h1>Prestation</h1>
-            <p>Bienvenue sur la page Prestation.</p>
+            {/*Section d'en-tête*/}
+            <HeadingSection title='Nos Prestations' />
+
+            {/*Section de présentation des prestations*/}
+            <BasicSection>
+                <p>
+                    Ici il y aura les différentes prestations. Elle serons présenté sous forme d'onglets avec la description, les tarifs, ect... Et il y aura un bouton de contact à la fin.
+                </p>
+                <Button text='Obtenir un devis' />
+            </BasicSection>
+            
         </div>
     );
 };
