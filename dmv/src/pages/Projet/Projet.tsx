06085ef5b1b32ba8203f07e70e18a59219d9a897
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-05-02 15:31:04 +0200

    ajout des projets sur la page projet

--- dmv/src/pages/Projet/Projet.tsx
+++ dmv/src/pages/Projet/Projet.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeadingSection from '../../components/sections/HeadingSection/HeadingSection';
 import BasicSection from '../../components/sections/BasicSection/BasicSection';
 import BackgroundHeading from '../../assets/pictures/background/heading.jpg';
+import stylesBasicSection from "../../components/sections/BasicSection/BasicSection.module.scss";
+import { ProjectType } from '../../projects/project.types';
+import { ProjectService } from '../../projects/ProjectService';
+import ProjectGrid from '../../components/grid/ProjectGrid/ProjectGrid';
 
 const Projet: React.FC = () => {
+    // État local pour stocker les projets à afficher
+        const [projects, setProjects] = useState<ProjectType[]>([]);
+        // Au montage du composant, on récupère les projets à afficher
+        useEffect(() => {
+            ProjectService.getAllProjects()
+                .then(setProjects)
+                .catch(console.error);
+        }, []);
+        
     return (
         <div>
             {/*Section d'en-tête*/}
             <HeadingSection title="Nos Projets" backgroundImage={BackgroundHeading}/>
 
             {/*Section de présentation des projets*/}
-            <BasicSection>
+            <BasicSection className={stylesBasicSection.sectionPage}>
 
                 <p>
-                    Ici il y aura l'intégralité des cards projets.
-                    La page projet, comportera 3 sections, une en-tête, une section de présentation et une section galerie.
+                    Faire un petit texte présentant les projets, faut vendre du reve la mon reuf !
                 </p>
+                
+                <ProjectGrid projects={projects} />
                 
             </BasicSection>
 
