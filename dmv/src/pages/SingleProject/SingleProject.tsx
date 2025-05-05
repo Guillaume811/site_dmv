f2dace09158233b1bca1e628200aa535d1d32928
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-05-05 11:05:24 +0200

    Mise en place du composant SingleProject

---
+++ dmv/src/pages/SingleProject/SingleProject.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ProjectType } from '../../projects/project.types';
+import { ProjectService } from '../../projects/ProjectService';
+import HeadingSection from '../../components/sections/HeadingSection/HeadingSection';
+import stylesTitlesSection from "../../components/sections/TitleSection/TitleSection.module.scss";
+import TitleSection from '../../components/sections/TitleSection/TitleSection';
+
+const SingleProject: React.FC = () => {
+
+    const { slug } = useParams(); // Récupère le slug de l'URL
+  const [project, setProject] = useState<ProjectType | null>(null);
+
+  useEffect(() => {
+    if (!slug) return;
+
+    ProjectService.getProjectBySlug(slug)
+      .then((data) => setProject(data || null))
+      .catch(console.error);
+  }, [slug]);
+
+  if (!project) return <p>Chargement ou projet introuvable.</p>;
+  
+    return (
+        <div>
+            <HeadingSection 
+                title={project.title} 
+                backgroundImage={project.pictureHeaderUrl}
+            />
+
+            <TitleSection
+                title={project.subtitle}
+                classNameSection={stylesTitlesSection.sectionPresentation}
+                classNameTitle={stylesTitlesSection.sectionPresentation__title}
+            >
+                <p>
+                    {project.description}
+                </p>
+
+            </TitleSection>
+        </div>
+        
+
+    );
+};
+
+export default SingleProject;
