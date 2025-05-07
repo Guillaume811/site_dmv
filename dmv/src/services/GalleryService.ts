d35e4f22713e830d267f68bbe46a13c35b84146b
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-05-07 18:43:30 +0200

    changement du path

--- dmv/src/services/GalleryService.ts
+++ dmv/src/services/GalleryService.ts
@@ -2,4 +2,4 @@ import { GalleryType } from "../types/gallery.types";
 import { GetDataService } from "./GetDataService";
 
 // Création de l'instance unique typée pour les projets
-export const GalleryService = new GetDataService<GalleryType>("/projects.json");
+export const GalleryService = new GetDataService<GalleryType>("/gallery.json");
