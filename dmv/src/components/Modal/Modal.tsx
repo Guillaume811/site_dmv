364bf068d1f63995eee867b323ce308494fb3366
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-05-08 15:12:53 +0200

    Ajout du modal et sont style

---
+++ dmv/src/components/Modal/Modal.tsx
@@ -0,0 +1,34 @@
+import { ReactNode, useEffect } from "react";
+import styles from "./Modal.module.scss";
+import TitleSection from "../sections/TitleSection/TitleSection";
+
+type ModalProps = {
+    children: ReactNode;
+    onClose: () => void;
+};
+
+const Modal: React.FC<ModalProps> = ({ onClose, children }) => {
+// Fermer avec Échap
+    useEffect(() => {
+        const handleEsc = (e: KeyboardEvent) => {
+            if (e.key === "Escape") onClose();
+        };
+        // Fermer avec clic en dehors de la modale
+        window.addEventListener("keydown", handleEsc);
+        return () => window.removeEventListener("keydown", handleEsc);
+    }, [onClose]);
+
+    return (
+        <div className={styles.overlay} onClick={onClose}>
+            <div className={styles.overlay__modal} onClick={(e) => e.stopPropagation()}>
+                <button className={styles.overlay__modal__close} onClick={onClose}>×</button>
+                <TitleSection title="Contact" >
+                    {children}
+                </TitleSection>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
