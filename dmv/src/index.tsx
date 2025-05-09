71777ed8fc6c207a7f7cb6ad6f2172f5eaf3d9d5
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-05-09 14:50:30 +0200

    création du fichier global.d.ts + importation

--- dmv/src/index.tsx
+++ dmv/src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { ModalProvider } from './components/Modal/ModalContext';
+import './types/global';
 
 const container = document.getElementById('root');
 
