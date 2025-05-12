a1ed3750a9dd6b6e2661959947c791c1896b8266
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-05-12 18:28:45 +0200

    création de la page legalNotice et son style

---
+++ dmv/src/pages/LegalNotice/LegalNotice.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LegalNotice: React.FC = () => {
+    return (
+        <div>
+            <h1>Legal Notice</h1>
+            <p>This is the legal notice page.</p>
+        </div>
+    );
+};
+
+export default LegalNotice;
