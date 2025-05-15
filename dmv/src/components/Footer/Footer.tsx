4d0bbf02de9b43c3593c3901aafb1798a9a1a877
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-05-15 22:20:04 +0200

    changement design logo

--- dmv/src/components/Footer/Footer.tsx
+++ dmv/src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from "./Footer.module.scss";
 // Pictures and icons
-import logo from "../../assets/pictures/logo.png";
+import logoWhite from "../../assets/pictures/logo-white.png";
 import mailIcon from "../../assets/pictures/icons/mail.png";
 import phoneIcon from "../../assets/pictures/icons/phone.png";
 import linkedinIcon from "../../assets/pictures/icons/linkedin.png";
@@ -22,62 +22,60 @@ const Footer: React.FC = () => {
 
     return (
         <footer className={styles.footer}>
-            <div className={styles.footer__top}>
-                <div className={styles.footer__top__left}>                    
-                    <div className={styles.footer__top__left__social}>
+            <div className={styles.footer__left}>
+                <img src={logoWhite} alt="Logo DMV-Production" className={styles.footer__left__logo}/>
+                <p className={styles.footer__left__text}>
+                    &copy; {currentYear} DMV - Production.<br/>
+                    Made in Djénius
+                </p>
+            </div>
+
+            <div className={styles.footer__right}>
+                <div className={styles.footer__right__top}>
+                    <ul className={styles.footer__right__top__list}>
+                        {navLinks.map(({ id, label, to }) => (
+                            <li key={id} className={styles.footer__right__top__list__items}>
+                                <NavLink
+                                    to={to}
+                                    className={styles.footer__right__top__list__items__link}
+                                >
+                                    {label}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className={styles.footer__right__top__contact}>
+                        <h4 className={styles.footer__right__top__contact__title}>
+                            Contactez-nous :
+                        </h4>
+                        <div className={styles.footer__right__top__contact__button}>
+                            <img
+                                src={mailIcon}
+                                alt="Contacter par email"
+                                className={styles.footer__right__top__contact__button__icon}
+                                onClick={() => open(<ContactForm />)}
+                                role="button"
+                            />
+                            <a href="">
+                                <img src={phoneIcon} alt="Appeler DMV - Production" className={styles.footer__right__top__contact__button__icon} />
+                            </a>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className={styles.footer__right__bottom}>
+                    <h4 className={styles.footer__right__bottom__title}>
+                        Suivez-nous :
+                    </h4>
+                    <div className={styles.footer__right__bottom__social}>
                         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                            <img src={instagramIcon} alt="Instagram" className={styles.footer__top__left__social__icon} />
+                            <img src={instagramIcon} alt="Instagram" className={styles.footer__right__bottom__social__icon} />
                         </a>
                         <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                            <img src={linkedinIcon} alt="LinkedIn" className={styles.footer__top__left__social__icon} />
-                        </a>
-                    </div>                    
-                </div>
-                <div className={styles.footer__top__center}>
-                    <ul className={styles.footer__top__center__list}>
-                    {navLinks.map(({ id, label, to }) => (
-                        <li key={id} className={styles.footer__top__center__list__items}>
-                            <NavLink
-                                to={to}
-                                className={styles.footer__top__center__list__items__link}
-                            >
-                                {label}
-                            </NavLink>
-                        </li>
-                    ))}
-                    </ul> 
-                </div>
-                <div className={styles.footer__top__right}>
-                    <div className={styles.footer__top__right__contact}>
-                        <img
-                            src={mailIcon}
-                            alt="Contacter par email"
-                            className={styles.footer__top__right__contact__icon}
-                            onClick={() => open(<ContactForm />)}
-                            role="button"
-                        />
-                        <a href="">
-                            <img src={phoneIcon} alt="Appeler DMV - Production" className={styles.footer__top__right__contact__icon} />
+                            <img src={linkedinIcon} alt="LinkedIn" className={styles.footer__right__bottom__social__icon} />
                         </a>
                     </div> 
-                </div> 
-            </div>
-            <div className={styles.footer__bottom}>
-                <p className={styles.footer__bottom__text}>
-                    &copy; {currentYear} DMV - Production. Made in Djénius
-                </p>
-                <ul className={styles.footer__bottom__list}>
-                    <li className={styles.footer__bottom__list__items}>
-                        <NavLink to='/legal' className={styles.footer__bottom__list__items__link}>
-                            Mentions Légales
-                        </NavLink>
-                    </li>
-                    <li className={styles.footer__bottom__list__items}>
-                        <NavLink to='/legal' className={styles.footer__bottom__list__items__link}>
-                            Confidentialité
-                        </NavLink>
-                    </li>
-                </ul>
+                </div>
             </div>
         </footer>
     );
