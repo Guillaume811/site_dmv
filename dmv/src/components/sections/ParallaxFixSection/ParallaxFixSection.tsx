e0b282ec47c6d44f2710f1f1d4f6414d66ef4e78
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-05-23 11:47:02 +0200

    docs: Ajout du commentaire

--- dmv/src/components/sections/ParallaxFixSection/ParallaxFixSection.tsx
+++ dmv/src/components/sections/ParallaxFixSection/ParallaxFixSection.tsx
@@ -6,6 +6,14 @@ type SectionParallaxFixProps = {
     children: React.ReactNode;
 };
 
+/* Component SectionParallaxFix
+* Receives "backgroundImage" and "children" as props from "SectionParallaxFixProps".
+
+* View TSX :
+* Returns a <div> styled with "styles.parallaxContainer".
+* Applies the "backgroundImage" as an inline background style.
+* Renders the "children" content inside the container.
+*/
 const SectionParallaxFix: React.FC<SectionParallaxFixProps> = ({ backgroundImage, children }) => {
     return (
         <div
