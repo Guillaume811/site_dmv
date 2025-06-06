74b4567711136d5efeff94caf4fd80f1bad779f8
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-06-06 16:29:04 +0200

    feat: ajout du composant ScrollToTop pour améliorer la navigation

--- dmv/src/index.tsx
+++ dmv/src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { ModalProvider } from './components/Modal/ModalContext';
 import './types/global.d';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 const container = document.getElementById('root');
 
@@ -19,6 +20,7 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
       <ModalProvider>
         <App />
       </ModalProvider>
