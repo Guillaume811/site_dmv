e353a2ded724e9aaa7d27c022e77842336dec422
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-06-06 14:09:06 +0200

    refactor: mise à jour du composant RowDiv pour utiliser des classes personnalisées et améliorer la structure du rendu

--- dmv/src/components/div/RowDiv/RowDiv.tsx
+++ dmv/src/components/div/RowDiv/RowDiv.tsx
@@ -1,30 +1,45 @@
 import React from 'react';
-import styles from "./RowDiv.module.scss";
 
 // Typage
 type RowDivProps = {
     leftContent: React.ReactNode;
     rightContent: React.ReactNode;
+    classNameRowDiv: string;
+    classNameRowDivLeft?: string;
+    classNameRowDivRight?: string;
 }
 
 /* Component RowDiv
-* Receives "leftContent" and "rightContent" as props from "RowDivProps".
+* Receives "leftContent", "rightContent", "classNameRowDiv", "classNameRowDivLeft", and "classNameRowDivRight" as props from "RowDivProps".
+* Determines if inner <div> wrappers should be used based on the presence of both "classNameRowDivLeft" and "classNameRowDivRight".
 
 * View TSX :
-* Returns a main <div> with the class "styles.rowDiv".
-* Inside, it has two child <div>s:
-  -> The first one uses "styles.rowDiv__left" to display the "leftContent".
-  -> The second one uses "styles.rowDiv__right" to display the "rightContent". 
+* Returns a main <div> with the class "classNameRowDiv".
+* If both inner class names are provided:
+  -> Wraps "leftContent" in a <div> with "classNameRowDivLeft".
+  -> Wraps "rightContent" in a <div> with "classNameRowDivRight".
+* If not, renders "leftContent" and "rightContent" directly without inner <div> wrappers.
 */
-const RowDiv: React.FC<RowDivProps> = ({ leftContent, rightContent }) => {
+const RowDiv: React.FC<RowDivProps> = ({ leftContent, rightContent, classNameRowDiv, classNameRowDivLeft, classNameRowDivRight }) => {
+    const hasInnerDivs = classNameRowDivLeft && classNameRowDivRight;
+
     return (
-        <div className={styles.rowDiv}>
-            <div className={styles.rowDiv__left}>
+        <div className={classNameRowDiv}>
+        {hasInnerDivs ? (
+            <>
+                <div className={classNameRowDivLeft}>
+                    {leftContent}
+                </div>
+                <div className={classNameRowDivRight}>
+                    {rightContent}
+                </div>
+            </>
+        ) : (
+            <>
                 {leftContent}
-            </div>
-            <div className={styles.rowDiv__right}>
                 {rightContent}
-            </div>
+            </>
+        )}
         </div>
     );
 };
