33c9e2e02a80e31cbae85f1aa39db85b0d9537c6
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-06-12 18:18:07 +0200

    feat: ajout de la gestion responsive avec AccordionTab et TabsPrestation dans le composant Prestation

--- dmv/src/pages/Prestation/Prestation.tsx
+++ dmv/src/pages/Prestation/Prestation.tsx
@@ -2,25 +2,31 @@ import React from 'react';
 import HeadingSection from '../../components/sections/HeadingSection/HeadingSection';
 import BasicSection from '../../components/sections/BasicSection/BasicSection';
 import TabsPrestation from '../../components/TabsPrestation/TabsPrestation';
+import AccordionTab from "../../components/AccordionTab/AccordionTab";
+import useIsMobile from "../../hooks/useIsMobile";
 import stylesBasicSection from "../../components/sections/BasicSection/BasicSection.module.scss";
 import BackgroundHeading from '../../assets/pictures/background/heading.jpg';
 
 /* Component Page Prestation
 * Render logic :
+* Uses the custom hook "useIsMobile" to check if the screen size is mobile and stores the result in "isMobile".
 
 * View TSX :
-* Displays a "HeadingSection" with the title "Nos Prestations" and a background image from "BackgroundHeading".
-* Inside "BasicSection", shows the "TabsPrestation" component which will likely display different prestation tabs.
-
-* Responsive :
+Returns a <div> that contains:
+  -> A "HeadingSection" with the title "Nos Prestations" and a background image from "BackgroundHeading".
+  -> A "BasicSection" styled with "stylesBasicSection.sectionPage".
+    -> If "isMobile" is true, displays the "AccordionTab" component.
+    -> If "isMobile" is false, displays the "TabsPrestation" component.
 */
 const Prestation: React.FC = () => {
+    const isMobile = useIsMobile();
+
     return (
         <div>
             <HeadingSection title='Nos Prestations' backgroundImage={BackgroundHeading} />
 
             <BasicSection className={stylesBasicSection.sectionPage}>
-                <TabsPrestation />
+                {isMobile ? <AccordionTab /> : <TabsPrestation />}
             </BasicSection>
             
         </div>
